5a9405f929d2ac49dc84818ed94018e439f2ee73
Dawson Hensel <dawsonhensel@gmail.com>
2024-06-10 20:39:56 -0700

    read file contents

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
-int main()
+void readFile(char *filepath)
 {
-    printf("Hello, World!");
+    int c = 0;
+    FILE *f = fopen(filepath, "rb");
+
+    if (f)
+    {
+        while ((c = getc(f)) != EOF)
+            putchar(c);
+        fclose(f);
+    }
+    else
+    {
+        printf("file '%s' was not found", filepath);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    char fpath;
+    if (argc > 1)
+    {
+        printf("filepath: '%s'\n", argv[1]);
+        readFile(argv[1]);
+    }
+    else
+    {
+        printf("usage: %s <filepath>\n", argv[0]);
+    }
 
     return 0;
 }
